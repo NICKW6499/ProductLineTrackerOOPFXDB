60bcc02c382b50ed0fde99284fb54d234f00c5c6
NICKW6499 <35817669+NICKW6499@users.noreply.github.com>
2019-09-28 22:37:28 -0400

    Add files via upload
    Style fixes and additional comments as well as bug removal.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,55 +6,51 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Group;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+
 /*
  *This file is the controller and has most methods stored here.
  * @author: Nicholis Wright
  * */
+
 public class Controller {
-  @FXML public Button recordProductionButton;
-  @FXML public Button addButton;
-  @FXML private ComboBox ComboBox;
+
+  public Button addButton;
+  public Button recordProductionButton;
+  @FXML private ComboBox<Integer> comboBox;
+
   /*
   * @author: Nicholis Wright
   *@param ActionEvent actionEvent: this param passes in the action taking place on the
   product add button
   * */
-  public void handleProductAddButton(ActionEvent actionEvent) {
-    System.out.println("hopefully this works");
-    final String JDBC_DRIVER = "org.h2.Driver";
-    final String DB_URL = "jdbc:h2:./H2/RES";
 
-    // data base credentials
-    Connection conn;
-    Statement stmt;
+  public void handleProductAddButton() {
+    System.out.println("hopefully this works");
+    dataInsert();
   }
+
   /*
   * @author: Nicholis Wright
   *@param ActionEvent actionEvent: this param passes in the action taking place on the
   record product button
   * */
-  public void handleRecordProductionButton(ActionEvent actionEvent) {
-    System.out.println("hopefully this works");
-    final String JDBC_DRIVER = "org.h2.Driver";
-    final String DB_URL = "jdbc:h2:./H2/RES";
 
-    // data base credentials
-    Connection conn;
-    Statement stmt;
+  public void handleRecordProductionButton() {
+    System.out.println("hopefully this works");
+    dataInsert();
   }
+
   /*
    * @author: Nicholis Wright
    *This method initializes the connection to the data base and inserts items into it.
    * */
-  public void DataInsert() {
-    final String JDBC_DRIVER = "org.h2.Driver";
-    final String DB_URL = "jdbc:h2:./H2/RES";
+
+  private void dataInsert() {
+    final String jdbc_driver = "org.h2.Driver";
+    final String dbUrl = "jdbc:h2:./H2/RES";
 
     // data base credentials
     Connection conn;
@@ -62,10 +58,10 @@ public class Controller {
 
     try {
       // STEP 1: Register JDBC driver
-      Class.forName(JDBC_DRIVER);
+      Class.forName(jdbc_driver);
 
       // STEP 2: Open a connection
-      conn = DriverManager.getConnection(DB_URL);
+      conn = DriverManager.getConnection(dbUrl);
 
       // STEP 3: Execute a query
       stmt = conn.createStatement();
@@ -79,19 +75,24 @@ public class Controller {
       // STEP 4: Clean-up environment
       stmt.close();
       conn.close();
+
       // catches/handles exception errors
     } catch (ClassNotFoundException | SQLException e) {
       e.printStackTrace();
     }
   }
+
   /*
    * @author: Nicholis Wright
-   *This method populates the combo box with decimal numbers 1-10
+   *This method populates the combo box with decimal numbers 1-10.
+   * @param none: no parameters
    * */
+
   public void initialize() {
-    ObservableList populateNum = FXCollections.observableArrayList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-    ComboBox.setItems(populateNum);
-    ComboBox.getSelectionModel().selectFirst();
-    ComboBox.setEditable(true);
+    ObservableList<Integer> populateNum =
+        FXCollections.observableArrayList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    comboBox.setItems(populateNum);
+    comboBox.getSelectionModel().selectFirst();
+    comboBox.setEditable(true);
   }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,17 +5,20 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 /*
  *This is the main file of the program.
  * @author: Nicholis Wright
  *
  * */
+
 public class Main extends Application {
   /*
    *This method creates the scene.
    * @author: Nicholis Wright
    *@param Stage primary stage: sets scene.
    * */
+
   @Override
   public void start(Stage primaryStage) throws Exception {
     Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
@@ -23,11 +26,12 @@ public class Main extends Application {
     primaryStage.setScene(new Scene(root, 400, 400));
     primaryStage.show();
   }
+
   /*
    * @author: Nicholis Wright
    *@param String[args]: main array that holds commands
-   *@param
    * */
+
   public static void main(String[] args) {
     launch(args);
   }
